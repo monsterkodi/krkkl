e447411c720e6412b8755580ae697f0e1012524d
monsterkodi <monsterkodi@gmx.net>
2015-07-28 19:24:06 +0200

    init

---
+++ krkkl/krkklView.h
@@ -0,0 +1,13 @@
+//
+//  krkklView.h
+//  krkkl
+//
+//  Created by kodi on 28/07/15.
+//  Copyright (c) 2015 kodi. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface krkklView : ScreenSaverView
+
+@end
