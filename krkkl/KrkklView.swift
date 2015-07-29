52863a454908ba399342ce063ea961b4398d4444
Erik Doernenburg <erik@doernenburg.com>
2015-07-29 16:51:35 +0200

    using tuple to save cube pos

--- krkkl/KrkklView.swift
+++ krkkl/KrkklView.swift
@@ -3,12 +3,12 @@ import ScreenSaver
 
 class KrkklView : ScreenSaverView {
     
-    var sizex = 0
-    var sizey = 0
-    var x = 0
-    var y = 0
-    var lastPos:UInt = 0
-    var nextPos:UInt = 0
+    var numrows = 100
+    
+    var pos: (x: Int, y: Int) = (0, 0)
+    
+    var lastPos: UInt = 0
+    var nextPos: UInt = 0
     
     override init(frame: NSRect, isPreview: Bool) {
         super.init(frame: frame, isPreview: isPreview)
@@ -23,8 +23,8 @@ class KrkklView : ScreenSaverView {
     }
 
     override func startAnimation() {
-        setup()
         super.startAnimation()
+        setup()
         setAnimationTimeInterval(1.0 / 30.0)
         needsDisplay = true
     }
@@ -34,11 +34,7 @@ class KrkklView : ScreenSaverView {
     }
     
     func setup() {
-        var numrows = 100
-        var height = Int(bounds.size.height)
-        numrows = min(numrows, height/2)
-        sizey = height/numrows
-        sizex = Int(sin(M_PI/3) * Double(sizey))
+        numrows = min(numrows, Int(bounds.size.height)/2)
     }
     
     override func drawRect(rect: NSRect) {
@@ -48,24 +44,26 @@ class KrkklView : ScreenSaverView {
         bPath.fill()
     }
     
-    
     override func animateOneFrame() {
-//        needsDisplay = true
-//        setNeedsDisplayInRect(bounds)
         let context = window!.graphicsContext
         NSGraphicsContext.setCurrentContext(context)
-        nextCube()
+        drawNextCube()
         context?.flushGraphics()
     }
     
-    func nextCube()
+    func cubeSize() -> (w: Int, h: Int) {
+        var height = Int(bounds.size.height)/numrows
+        return (Int(sin(M_PI/3) * Double(height)), height)
+    }
+    
+    func drawNextCube()
     {
         var skip = -1
-        var width = Int(bounds.size.width)
-        var height = Int(bounds.size.height)
+        
+        let (cw, ch) = cubeSize()
 
-        var nx = Int(width/sizex)
-        var ny = Int(height/sizey)
+        let nx = Int(bounds.size.width)/cw
+        let ny = Int(bounds.size.height)/ch
 
         nextPos = UInt(arc4random_uniform(6))
         lastPos = nextPos
@@ -73,43 +71,43 @@ class KrkklView : ScreenSaverView {
         switch nextPos
         {
         case 0: // up
-                y += 1
+                pos.y += 1
             
         case 1: // right
-                if (x%2)==1 {
-                    y -= 1
+                if (pos.x%2)==1 {
+                    pos.y -= 1
                 }
-                x += 1
+                pos.x += 1
 
         case 2: // left
-                if (x%2)==1 {
-                    y -= 1
+                if (pos.x%2)==1 {
+                    pos.y -= 1
                 }
-                x -= 1
+                pos.x -= 1
 
         case 3: // down
-                y -= 1
+                pos.y -= 1
                 skip = 0 // dont paint top
 
         case 4: // back left
-                if (x%2)==0 {
-                    y += 1
+                if (pos.x%2)==0 {
+                    pos.y += 1
                 }
-                x -= 1
+                pos.x -= 1
                 skip = 2 // dont paint right
 
         case 5: // back right
-                if (x%2)==0 {
-                    y += 1
+                if (pos.x%2)==0 {
+                    pos.y += 1
                 }
-                x += 1
+                pos.x += 1
                 skip = 1 // dont paint left
             
             default:
                 break
         }
 
-        if (x < 1 || y < 2 || x > nx-1 || y > ny-1) {
+        if (pos.x < 1 || pos.y < 2 || pos.x > nx-1 || pos.y > ny-1) {
 //            if reset == "center" {
 //                x = cx
 //                y = cy
@@ -120,42 +118,35 @@ class KrkklView : ScreenSaverView {
 //            }
 //            else if reset == "wrap" {
             if (true) {
-                if (x < 1) {
-                    x = nx-1
+                if (pos.x < 1) {
+                    pos.x = nx-1
                 }   
-                else if (x > nx-1)
+                else if (pos.x > nx-1)
                 {   
-                    x = 1
+                    pos.x = 1
                 }
-                if (y < 2)
+                if (pos.y < 2)
                 {
-                    y = ny-1
+                    pos.y = ny-1
                 }
-                else if (y > ny-1)
+                else if (pos.y > ny-1)
                 {
-                    y = 2
+                    pos.y = 2
                 }
             }
             lastPos = UInt(arc4random_uniform(6))
         }
 
-        drawCube(xi: x, yi: y, color: NSColor.greenColor(), skip: skip)
+        drawCube(color: NSColor.greenColor(), skip: skip)
     }
     
-    func drawCube(#xi: Int, yi: Int, color: NSColor, skip: Int)
+    func drawCube(#color: NSColor, skip: Int)
     {
-        let h = sizey
-        let w = sizex
+        let (w, h) = cubeSize()
         let s = h/2
-        
-        let x = xi * w
-        var y = yi * h
-        
-        if (xi%2 == 1)
-        {
-            y -= h/2
-        }
-        
+        let x = pos.x*w
+        let y = (pos.x%2 == 0) ? (pos.y*h) : (pos.y*h - h/2)
+     
         if skip != 0 { // top
             color.set()
             let path = NSBezierPath()
