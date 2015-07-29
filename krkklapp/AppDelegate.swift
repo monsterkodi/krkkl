6e6e832288789112c0b5e8f9cb1c1140adc5ea6a
monsterkodi <monsterkodi@gmx.net>
2015-07-29 23:32:46 +0200

    app resize

--- krkklapp/AppDelegate.swift
+++ krkklapp/AppDelegate.swift
@@ -3,32 +3,41 @@ import Cocoa
 import ScreenSaver
 
 @NSApplicationMain
-class AppDelegate: NSObject {
-    
+class AppDelegate: NSObject
+{
     @IBOutlet var window: NSWindow!
     
-    deinit {
+    var view: KrkklView?
+    
+    deinit
+    {
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
 }
 
-
-extension AppDelegate: NSApplicationDelegate {
-    func applicationDidFinishLaunching(notification: NSNotification) {
-
-        let view = KrkklView(frame: CGRectZero, isPreview: false)
-        view.autoresizingMask = NSAutoresizingMaskOptions.ViewWidthSizable | NSAutoresizingMaskOptions.ViewHeightSizable
-        view.frame = window.contentView.bounds
-        window.contentView.addSubview(view)
+extension AppDelegate: NSApplicationDelegate
+{
+    func applicationDidFinishLaunching(notification: NSNotification)
+    {
+        view = KrkklView(frame: CGRectZero, isPreview: false)
+        view!.autoresizingMask = NSAutoresizingMaskOptions.ViewWidthSizable | NSAutoresizingMaskOptions.ViewHeightSizable
+        view!.frame = window.contentView.bounds
+        window.contentView.addSubview(view!)
         
-        view.startAnimation()
-        NSTimer.scheduledTimerWithTimeInterval(view.animationTimeInterval(), target: view, selector: "animateOneFrame", userInfo: nil, repeats: true)
+        view!.startAnimation()
+        NSTimer.scheduledTimerWithTimeInterval(view!.animationTimeInterval(), target: view!, selector: "animateOneFrame", userInfo: nil, repeats: true)
     }
 }
 
-
-extension AppDelegate: NSWindowDelegate {
-    func windowWillClose(notification: NSNotification) {
+extension AppDelegate: NSWindowDelegate
+{
+    func windowWillClose(notification: NSNotification)
+    {
         NSApplication.sharedApplication().terminate(window)
+    }
+    
+    func windowDidResize(notification: NSNotification)
+    {
+        view?.setup()
     }
 }
